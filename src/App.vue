cc1ff99c2428891c690569647106398f7539dec3
yuanxingzhi <760946378@qq.com>
2019-12-20 18:10:40 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="app">
+    <div class="login_container">
+      <div class="login_box">
+        <div class="avatar_box">
+          <img src="../assets/logo.png">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'app'
 }
 </script>
 
 <style>
+
 </style>
