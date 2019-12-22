98e210395c3a3603fd8b8e0d3e68b1c69f1e3fea
17868803825 <yxz0921>
2019-12-22 16:02:38 +0800

    相关登录页面

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,14 +2,65 @@
   <div class="login_container">
     <div class="login_box">
       <div class="avatar_box">
-        <img src="../assets/logo.png">
+        <img src="../assets/logo.jpg">
+      </div>
+      <div>
+        <el-form ref="loginFormRef" label-width="0px" class="login_form" :model="loginForm" :rules="ruleForm">
+          <!--账户-->
+          <el-form-item prop="account">
+            <el-input prefix-icon="iconfont icon-account" v-model="loginForm.account" ></el-input>
+          </el-form-item>
+          <!--密码-->
+          <el-form-item prop="password">
+            <el-input type="password" prefix-icon="iconfont icon-password" v-model="loginForm.password" ></el-input>
+          </el-form-item>
+          <!--按钮区域-->
+          <el-form-item class="login_button">
+            <el-button type="primary" @click="login" plain>登陆</el-button>
+            <el-button type="info" @click="resetLoginForm" plain>重置</el-button>
+          </el-form-item>
+        </el-form>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      // 登陆表单绑定数据
+      loginForm: {
+        account: 'admin',
+        password: '123456'
+      },
+      // 表单验证
+      ruleForm: {
+        account: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 5, max: 20, message: '长度在 5 到 20 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
+      }
+
+    }
+  },
+  methods: {
+    // 点击重置按钮，重置表单数据
+    resetLoginForm () {
+      this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      this.$refs.loginFormRef.validate(valid => {
+        if (!valid) return 0
+        this.$http.post('login', this.loginForm)
+      })
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
@@ -46,5 +97,16 @@ export default {}
         background-color: #eee
       }
     }
+  }
+  .login_button{
+    display: flex;
+    justify-content: flex-end;
+  }
+  .login_form{
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
   }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,7 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, FormItem, Form, Input } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
